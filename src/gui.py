98fcddb3a8af7eab7492c4b8beb642b9c10575c1
jakub <jakubprzybylski676@gmail.com>
2026-08-12 11:34:26 +0200

    improve label positioning

--- src/gui.py
+++ src/gui.py
@@ -11,23 +11,23 @@ def run_app():
     root.geometry("300x300")
 
     # Space to enter the name of ther city
-    label_input = ctk.CTkEntry(root, placeholder_text="Wpisz miasto", font=("Robot", 16, "bold"))
-    label_input.grid(row=0, column=0, padx=10, pady=5)
+    label_input = ctk.CTkEntry(root, placeholder_text="Enter the city", font=("Robot", 16, "bold"))
+    label_input.grid(row=0, column=0, padx=10, pady=5, sticky="ew")
 
     # Button to search the city
-    label_button = ctk.CTkButton(root, text="Szukaj", font=("Roboto", 16, "bold"))
+    label_button = ctk.CTkButton(root, text="Search", font=("Roboto", 16, "bold"))
     label_button.grid(row=0, column=1, padx=10, pady=5)
 
     # text with the name of the city
     label_name = ctk.CTkLabel(root, text="city", font=("Roboto", 16,"bold"))
-    label_name.grid(row=1, column=0, padx=20, pady=5)
+    label_name.grid(row=1, column=0, columnspan=2, padx=20, pady=5)
 
     # temperature in the city
     label_temperature = ctk.CTkLabel(root, text="20", font=("Roboto", 16,"bold"))
-    label_temperature.grid(row=2, column=0, padx=20, pady=5)
+    label_temperature.grid(row=2, column=0, columnspan=2, padx=20, pady=5)
 
     # description of the weather in the city
     label_description = ctk.CTkLabel(root, text="description", font=("Roboto", 16,"bold"))
-    label_description.grid(row=3, column=0, padx=20, pady=5)
+    label_description.grid(row=3, column=0, columnspan=2, padx=20, pady=5)
 
     root.mainloop()
