2fb584ad17679ff16695c5c86f37dd3eaaf68610
jakub <jakubprzybylski676@gmail.com>
2026-08-12 12:10:50 +0200

    change the size of labels

--- src/gui.py
+++ src/gui.py
@@ -19,21 +19,21 @@ def run_app():
     label_button.grid(row=0, column=1, padx=10, pady=5)
 
     # text with the name of the city
-    label_name = ctk.CTkLabel(root, text="city", font=("Roboto", 16,"bold"))
+    label_name = ctk.CTkLabel(root, text="city", font=("Roboto", 32,"bold"))
     label_name.grid(row=1, column=0, columnspan=2, padx=20, pady=5)
 
     # temperature in the city
-    label_temperature = ctk.CTkLabel(root, text="20", font=("Roboto", 16,"bold"))
+    label_temperature = ctk.CTkLabel(root, text="20", font=("Roboto", 32,"bold"))
     label_temperature.grid(row=2, column=0, columnspan=2, padx=20, pady=5)
 
     # description of the weather in the city
-    label_description = ctk.CTkLabel(root, text="description", font=("Roboto", 16,"bold"))
+    label_description = ctk.CTkLabel(root, text="description", font=("Roboto", 32,"bold"))
     label_description.grid(row=3, column=0, columnspan=2, padx=20, pady=5)
 
-    label_perceivedTemperature = ctk.CTkLabel(root, text="20", font=("Roboto", 10))
+    label_perceivedTemperature = ctk.CTkLabel(root, text="20", font=("Roboto", 16))
     label_perceivedTemperature.grid(row=4, column=0, padx=20, pady=5)
 
-    label_wind = ctk.CTkLabel(root, text="20", font=("Roboto", 10))
+    label_wind = ctk.CTkLabel(root, text="20", font=("Roboto", 16))
     label_wind.grid(row=4, column=1, padx=20, pady=5)
 
     root.mainloop()
