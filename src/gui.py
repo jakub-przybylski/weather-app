a2afb08e70f8572946dfaf78e615c66a051a548b
jakub <jakubprzybylski676@gmail.com>
2026-08-12 10:35:47 +0200

    update in gui/new settings

--- src/gui.py
+++ src/gui.py
@@ -6,9 +6,11 @@ def run_app():
     ctk.set_default_color_theme("blue")
     
     root = ctk.CTk()
+    root.grid_columnconfigure(0, weight=1)
     root.title("Weather App")
     root.geometry("300x300")
 
-    label_text = ctk.CtkLabel(root, text="", font=("Roboto", 16,"bold") )
+    label_text = ctk.CTkLabel(root, text="Today is...", font=("Roboto", 16,"bold"))
+    label_text.grid(row=0, column=0, padx=20, pady=5)
 
     root.mainloop()
