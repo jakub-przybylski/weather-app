30cc2789987a1c554620f0bf588b3c7b9bf24cea
jakub <jakubprzybylski676@gmail.com>
2026-08-12 11:04:10 +0200

    next modifications with gui

--- src/gui.py
+++ src/gui.py
@@ -10,7 +10,16 @@ def run_app():
     root.title("Weather App")
     root.geometry("300x300")
 
+    # Space to enter the name of ther city
+    label_input = ctk.CTkEntry(root, placeholder_text="Wpisz miasto", font=("Robot", 16, "bold"))
+    label_input.grid(row=0, column=0, padx=10, pady=5)
+
+    # Button to search the city
+    label_button = ctk.CTkButton(root, text="Szukaj", font=("Roboto", 16, "bold"))
+    label_button.grid(row=0, column=1, padx=10, pady=5)
+
+    # text with basic information about the city
     label_text = ctk.CTkLabel(root, text="Today is...", font=("Roboto", 16,"bold"))
-    label_text.grid(row=0, column=0, padx=20, pady=5)
+    label_text.grid(row=1, column=0, padx=20, pady=5)
 
     root.mainloop()
