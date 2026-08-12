8dc34eebaf5b68a02fdab42b9f1f6d9615ffb3a2
jakub <jakubprzybylski676@gmail.com>
2026-08-12 12:16:22 +0200

    modifications in gui

--- src/gui.py
+++ src/gui.py
@@ -10,30 +10,32 @@ def run_app():
     root.title("Weather App")
     root.geometry("300x300")
 
-    # Space to enter the name of ther city
+    # space to enter the name of ther city
     label_input = ctk.CTkEntry(root, placeholder_text="Enter the city", font=("Robot", 16, "bold"))
     label_input.grid(row=0, column=0, padx=10, pady=5, sticky="ew")
 
-    # Button to search the city
+    # button to search the city
     label_button = ctk.CTkButton(root, text="Search", font=("Roboto", 16, "bold"))
     label_button.grid(row=0, column=1, padx=10, pady=5)
 
     # text with the name of the city
     label_name = ctk.CTkLabel(root, text="city", font=("Roboto", 32,"bold"))
-    label_name.grid(row=1, column=0, columnspan=2, padx=20, pady=5)
+    label_name.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
 
     # temperature in the city
     label_temperature = ctk.CTkLabel(root, text="20", font=("Roboto", 32,"bold"))
-    label_temperature.grid(row=2, column=0, columnspan=2, padx=20, pady=5)
+    label_temperature.grid(row=2, column=0, columnspan=2, padx=10, pady=5)
 
     # description of the weather in the city
     label_description = ctk.CTkLabel(root, text="description", font=("Roboto", 32,"bold"))
-    label_description.grid(row=3, column=0, columnspan=2, padx=20, pady=5)
+    label_description.grid(row=3, column=0, columnspan=2, padx=10, pady=5)
 
-    label_perceivedTemperature = ctk.CTkLabel(root, text="20", font=("Roboto", 16))
-    label_perceivedTemperature.grid(row=4, column=0, padx=20, pady=5)
+    # perceived temperature
+    label_perceivedTemperature = ctk.CTkLabel(root, text="Perceived: 20", font=("Roboto", 16))
+    label_perceivedTemperature.grid(row=4, column=0, padx=10, pady=5)
 
-    label_wind = ctk.CTkLabel(root, text="20", font=("Roboto", 16))
-    label_wind.grid(row=4, column=1, padx=20, pady=5)
+    # wind
+    label_wind = ctk.CTkLabel(root, text="Wind: 20", font=("Roboto", 16))
+    label_wind.grid(row=4, column=1, padx=10, pady=5)
 
     root.mainloop()
