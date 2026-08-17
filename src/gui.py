5249da1e15dc961fd8acbbbf05e217091c6b2e5c
jakub <jakubprzybylski676@gmail.com>
2026-08-17 21:09:08 +0200

    appearance mode dark

--- src/gui.py
+++ src/gui.py
@@ -2,7 +2,7 @@ import customtkinter as ctk
 
 def run_app():
 
-    ctk.set_appearance_mode("light")
+    ctk.set_appearance_mode("dark")
     ctk.set_default_color_theme("blue")
     
     root = ctk.CTk()
