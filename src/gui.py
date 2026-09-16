470a81fc138b76e633a8c07c0c2c5f1d1ce5dd3c
Jakub <jakubprzybylski676@gmail.com>
2026-09-16 16:19:23 +0200

    changing the structure and size of gui

--- src/gui.py
+++ src/gui.py
@@ -8,41 +8,42 @@ def run_app():
     
     root = ctk.CTk()
     root.grid_columnconfigure(0, weight=1)
+    root.grid_columnconfigure(1, weight=1)
     root.title("Weather App")
-    root.geometry("300x300")
+    root.geometry("400x400")
 
     # space to enter the name of ther city
-    label_input = ctk.CTkEntry(root, placeholder_text="Enter the city", font=("Segoe UI", 16, "bold"))
-    label_input.grid(row=0, column=0, padx=10, pady=5, sticky="ew")
+    label_input = ctk.CTkEntry(root, placeholder_text="Enter the city", font=("Segoe UI", 20, "bold"))
+    label_input.grid(row=0, column=0, columnspan=2, padx=10, pady=5, sticky="ew")
 
     # button to search the city
-    label_button = ctk.CTkButton(root, text="Search", font=("Segoe UI", 16, "bold"))
-    label_button.grid(row=0, column=1, padx=10, pady=5)
+    label_button = ctk.CTkButton(root, text="Search", font=("Segoe UI", 20, "bold"))
+    label_button.grid(row=1, column=0, columnspan=2, padx=10, pady=5, sticky="ew")
 
     # text with the name of the city
-    label_name = ctk.CTkLabel(root, text="city", font=("Segoe UI", 32,"bold"))
-    label_name.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
+    label_name = ctk.CTkLabel(root, text="city", font=("Segoe UI", 40,"bold"))
+    label_name.grid(row=2, column=0, columnspan=2, padx=10, pady=5, sticky="ew")
 
     # temperature in the city
-    label_temperature = ctk.CTkLabel(root, text="20", font=("Segoe UI", 32,"bold"))
-    label_temperature.grid(row=2, column=0, columnspan=2, padx=10, pady=5)
+    label_temperature = ctk.CTkLabel(root, text="20", font=("Segoe UI", 40,"bold"))
+    label_temperature.grid(row=3, column=0, columnspan=2, padx=10, pady=5)
 
     # description of the weather in the city
     label_description = ctk.CTkLabel(root, text="description", font=("Segoe UI", 32,"bold"))
-    label_description.grid(row=3, column=0, columnspan=2, padx=10, pady=5)
+    label_description.grid(row=4, column=0, columnspan=2, padx=10, pady=5, sticky="ew")
 
     # image 
-    description_image = ctk.CTkImage(light_image=Image.open("assets/face.png"), dark_image=Image.open("assets/face.png"), size=(100, 50))
+    description_image = ctk.CTkImage(light_image=Image.open("assets/face.png"), dark_image=Image.open("assets/face.png"), size=(140, 70))
 
     label_image = ctk.CTkLabel(root, text="", image=description_image)
-    label_image.grid(row=4, column=0, columnspan=2, padx=10, pady=3)
+    label_image.grid(row=5, column=0, columnspan=2, padx=10, pady=3, sticky="ew")
 
     # perceived temperature
-    label_perceivedTemperature = ctk.CTkLabel(root, text="Perceived: 20", font=("Segoe UI", 16))
-    label_perceivedTemperature.grid(row=5, column=0, padx=10, pady=5)
+    label_perceivedTemperature = ctk.CTkLabel(root, text="Perceived: 20", font=("Segoe UI", 20))
+    label_perceivedTemperature.grid(row=6, column=0, padx=10, pady=5, sticky="ew")
 
     # wind
-    label_wind = ctk.CTkLabel(root, text="Wind: 20", font=("Segoe UI", 16))
-    label_wind.grid(row=5, column=1, padx=10, pady=5)
+    label_wind = ctk.CTkLabel(root, text="Wind: 20", font=("Segoe UI", 20))
+    label_wind.grid(row=6, column=1, padx=10, pady=5, sticky="ew")
 
     root.mainloop()
